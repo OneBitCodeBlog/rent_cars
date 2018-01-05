d2c3493f85c373024f59b975a0c12e4d8166e8da
Daniel Moreto <dfmoreto@gmail.com>
2018-01-05 14:01:40 -0200

    Add callback at RentalMailer

--- app/mailers/rental_mailer.rb
+++ app/mailers/rental_mailer.rb
@@ -1,6 +1,15 @@
 class RentalMailer < ApplicationMailer
+  before_action :load_rental
+
   def confirmation
-    @rental = params[:rental]
     mail to: @rental.user.email, subject: "Rental Confirmation of #{@rental.car.name}"
+  end
+
+
+  private
+
+
+  def load_rental
+    @rental = params[:rental]
   end
 end
